915737047aab3ad0bcd7e65e7fc7d0883da786aa
Silvio Carrera <carrera.silvio@gmail.com>
2020-12-24 10:42:00 -0300

    Finished Day3

--- AdventOfCode2020/Day3.h
+++ AdventOfCode2020/Day3.h
@@ -1,7 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
 #pragma once
 class Day3
 {
 public:
-	int run();
+	void run(string inputPath) {
+
+		//Read input, parse and calc solution
+		char tree = '#';
+		string openspace = ".";
+		int indexIncrement = 3;
+		int totalTrees = 0;
+
+		fstream newfile;
+		newfile.open(inputPath, ios::in);
+		if (newfile.is_open())
+		{
+			string line;
+			int indexPos = 0;
+			while (getline(newfile, line))
+			{
+				cout << line << "\n";
+				if (line[indexPos] == tree) {
+					totalTrees++;
+				}
+				indexPos = (indexPos + indexIncrement) % line.length();
+			}
+			newfile.close();
+		}
+
+		cout << "Total trees is " << totalTrees << endl;
+
+	}
 };
 
--- AdventOfCode2020/main.cpp
+++ AdventOfCode2020/main.cpp
@@ -7,13 +7,17 @@ using namespace std;
 
 int main()
 {
-	cout << "Day 1" << endl;
-	Day1 day1;
-	day1.run();
+	//cout << "Day 1" << endl;
+	//Day1 day1;
+	//day1.run();
 
 	//cout << "Day 2" << endl;
 	//Day2 day2;
 	//day2.run("day2_input.txt");
+
+	cout << "Day 3" << endl;
+	Day3 day3;
+	day3.run("day3_input.txt");
 
 	return 0;
 }
