1d42227add5c1c57c7796326289e3d318cf79e33
Silvio Carrera <carrera.silvio@gmail.com>
2020-12-24 10:48:07 -0300

    WIP Day4

--- AdventOfCode2020/Day4.h
+++ AdventOfCode2020/Day4.h
@@ -1,5 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 #pragma once
 class Day4
 {
+public:
+	void run(string inputPath) {
+
+		//Read input, parse and calc solution
+		char tree = '#';
+		string openspace = ".";
+		int indexIncrement = 3;
+		int totalTrees = 0;
+
+		fstream newfile;
+		newfile.open(inputPath, ios::in);
+		if (newfile.is_open())
+		{
+			string line;
+			int indexPos = 0;
+			while (getline(newfile, line))
+			{
+				cout << line << "\n";
+				if (line[indexPos] == tree) {
+					totalTrees++;
+				}
+				indexPos = (indexPos + indexIncrement) % line.length();
+			}
+			newfile.close();
+		}
+
+		cout << "Total trees is " << totalTrees << endl;
+
+	}
 };
 
