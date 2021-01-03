254be0788be252b06e2c293a9fc16e90aba37c18
Silvio Carrera <carrera.silvio@gmail.com>
2021-01-03 15:57:09 -0300

    WIP Day18

--- AdventOfCode2020/Day18.h
+++ AdventOfCode2020/Day18.h
@@ -17,7 +17,8 @@ public:
     void run(string inputPath)
     {
         //Vars
-        stack<string> operationStack;
+        stack<char> stack;
+        int parenthesesDepth;
 
         //Extract nums 
         fstream newfile;
@@ -26,10 +27,48 @@ public:
         {
             string line;
             int lineId = 0;
+            int sum = 0;
 
             while (getline(newfile, line))
             {
-                
+                sum = 0;
+                for (auto entry : line)
+                {
+                    if (entry != '(' && entry != ')' && entry != '*' && entry != '+')
+                    {
+                        sum = //  2 + (1 + (3+1) + 2)
+                            /// stack #1
+                            /// 2 
+                            /// +
+                            ///  (
+                            ///  1 
+                            ///  + 
+                            ///   (
+                            ///   3
+                            ///   +
+                            ///   1
+                            ///   *
+                            ///   5
+                            ///   )
+                            ///  +
+                            ///  2
+                            ///  )
+                            /// stack #2
+                            /// 2 
+                            /// +
+                            /// (
+                            /// 1 
+                            /// + 
+                            /// (
+                            /// 3
+                            /// +
+                            /// 1
+                            /// )
+                            /// +
+                            /// 2
+                    }
+
+                }
 
                 //cout << lineId << " line " << line << endl;
                 lineId++;
